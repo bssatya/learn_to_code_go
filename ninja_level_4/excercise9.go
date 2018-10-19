f290a7c138077126f016f4fcf137b13a9fdac5db
Satyakumar BS <bs.satya@gmail.com>
2018-10-19 19:34:58 +0530

    program to demonstrate adding a new key/value to existing map

---
+++ ninja_level_4/excercise9.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func main () {
+	ms := map[string][]string{
+		"bond_james": []string{"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
+		"no_dr": []string{"Being Evil", "Ice creams", "Sunsets"},
+	}
+
+	ms["fleming_ian"] = []string{"Steaks", "Cigars", "Espionage"}
+
+	for k, kv := range ms {
+		fmt.Println("Key is", k, "Value is", kv)
+		for i, v := range kv {
+			fmt.Println("\tValue at index", i, "is", v)
+		}
+	}
+}
