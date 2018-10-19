dbd80d8029cb185a5a4297d03682379f5e86b37a
Satyakumar BS <bs.satya@gmail.com>
2018-10-19 10:15:15 +0530

    program to print 1-10000 using for - updated with few more types of for

--- ninja_level_3/exercise1.go
+++ ninja_level_3/exercise1.go
@@ -8,4 +8,18 @@ func main () {
 	for i := 1; i <= 10000; i++ {
 		fmt.Println(i)
 	}
+	
+	i := 1
+	for {
+		if (i <= 10000) {
+			break
+		}
+		fmt.Println(i)
+	}
+
+	for ;i <= 10000; {
+		fmt.Println(i)
+		i++
+	}
+
 }
