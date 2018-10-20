961232ef46defbde0384c02b8c9b2d47cc632e24
Satyakumar BS <bs.satya@gmail.com>
2018-10-21 02:33:10 +0530

    program to demonstrate race conditions while using go routines

---
+++ ninja_level_9/exercise3.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"runtime"
+)
+
+func main() {
+	var counter int
+	var wg sync.WaitGroup
+
+	printSysDetails()
+	gr := 100
+	wg.Add(gr)
+
+	for i := 0; i < gr; i++ {
+		go func() {
+			contr := counter
+			runtime.Gosched()
+			contr++
+			counter = contr
+			fmt.Println("Counter = ", counter)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Counter = ", counter)
+}
+
+func printSysDetails() {
+	fmt.Println("Runtime Arch:", runtime.GOARCH)
+	fmt.Println("Runtime Os:", runtime.GOOS)
+	fmt.Println("Runtime Num of Go Routine:", runtime.NumGoroutine())
+	fmt.Println("Runtime Num of CPU's:", runtime.NumCPU())
+}
