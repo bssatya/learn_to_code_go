cd3d5c5ee512ad132796b51d1ba3f384cfbf5767
Satyakumar BS <bs.satya@gmail.com>
2018-10-21 21:05:18 +0530

    program to demonstrate handling errors returns from functiona calls

---
+++ njnja_level_11/exercise1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := json.Marshal(p1)
+	if err != nil {
+		log.Fatalln("JSON didnt marshell", err)
+	}
+	fmt.Println(string(bs))
+
+}
