fc83218f9f06b6d973b801bc43e7c0874ae4a457
Satyakumar BS <bs.satya@gmail.com>
2018-10-21 20:01:17 +0530

    modified the program to use anonymous function

--- ninja_level_10/exercise6.go
+++ ninja_level_10/exercise6.go
@@ -6,20 +6,16 @@ import (
 
 func main() {
 	c := make(chan int)
-	go push(c)
-	pull(c)
-	fmt.Println("Done")
-}
+	go func() {
+		for i := 0; i < 100; i++ {
+			c<-i
+		}
+		close(c)
+	}()
 
-func push (c chan<- int) {
-	for i := 0; i < 100; i++ {
-		c<-i
-	}
-	close(c)
-}
-
-func pull(c <-chan int) {
 	for v := range c{
 		fmt.Println(v)
 	}
+
+	fmt.Println("Done")
 }
