466083bd0545665821a5eac566142683c667f6e5
Satyakumar BS <bs.satya@gmail.com>
2018-10-23 23:35:39 +0530

    created a root route and handler function. For now for any route, responding with hello world

--- web_application/main.go
+++ web_application/main.go
@@ -1,9 +1,34 @@
+// This is the name of our package
+// Everything with this pacakge name can see everything
+// else inside the same package, regardless of the file they are in
 package main
 
+// These are the libraies we are going to use
+// Both "fmt" and "net" are part of the Go standard library
 import (
+	// "fmt" has methods for formatted I/O operations (like printing to the console)
 	"fmt"
+	// The "net/http" library has methods to implement HTTP clients and servers
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Web application using Go...")
+	// The "HandleFunc" method accpet a path and a function as arguments
+	// (Yes, we can pass function as argument, and even treat them like variables in Go)
+	// However the handler function has to have the appropriate signature (as described by the "handler" function below)
+	http.HandleFunc("/", handler)
+
+	// After definign our server, we finally "listen and serve" on port 8080
+	// The second argument is the handler, which we will come to later on, for now it is left as nil and the
+	// handler defined above is (in "HandleFunc") used
+	http.ListenAndServe(":8080", nil)
+}
+
+// handler is our handler function. It has to follow the function signature of a ResponseWriter and Request type
+// as arguments.
+func handler(w http.ResponseWriter, r *http.Request){
+	// For this case, we will always pipe "Hello World" into the response writer
+	fmt.Fprintf(w, "Hello World\n")
+	
 }
