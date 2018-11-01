23587f5e9ce0e45bd5d9b5090af0eff9a73566c1
Satyakumar BS <bs.satya@gmail.com>
2018-11-01 22:14:34 +0530

    added the repeat n times logic, taking times as srgument. Added example tests as well

--- learn_go_with_tests/repeat.go
+++ learn_go_with_tests/repeat.go
@@ -1,8 +1,8 @@
 package main
 
 // func Repeat takes a string and returns a string which repeated form of the input string
-func Repeat(s string) (repeat string) {
-	for i := 0; i < 5; i++ {
+func Repeat(s string, times int) (repeat string) {
+	for i := 0; i < times; i++ {
 		repeat += s
 	}
 
--- learn_go_with_tests/repeat_test.go
+++ learn_go_with_tests/repeat_test.go
@@ -1,18 +1,26 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test_Repeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
 		t.Errorf("Got '%s', Want '%s'", repeated, expected)
 	}
 }
+func ExampleRepeat() {
+	sum := Repeat("s", 5)
+	fmt.Println(sum)
+	// Output: sssss
+}
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 6)
 	}
 }
