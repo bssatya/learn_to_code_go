8448603eb3d08e20908b3ce61ef79996a6efe649
Satyakumar BS <bs.satya@gmail.com>
2018-11-01 22:16:47 +0530

    changet the repeat func implimentation to use standard string Repeat function

--- learn_go_with_tests/repeat.go
+++ learn_go_with_tests/repeat.go
@@ -1,10 +1,9 @@
 package main
 
+import "strings"
+
 // func Repeat takes a string and returns a string which repeated form of the input string
 func Repeat(s string, times int) (repeat string) {
-	for i := 0; i < times; i++ {
-		repeat += s
-	}
-
+	repeat = strings.Repeat(s, times)
 	return
 }
