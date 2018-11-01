081b0f31c319f88d710a6549098eb7891dbc685e
Satyakumar BS <bs.satya@gmail.com>
2018-11-01 22:08:51 +0530

    added benchmark tests

--- learn_go_with_tests/repeat_test.go
+++ learn_go_with_tests/repeat_test.go
@@ -10,3 +10,9 @@ func Test_Repeat(t *testing.T) {
 		t.Errorf("Got '%s', Want '%s'", repeated, expected)
 	}
 }
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
