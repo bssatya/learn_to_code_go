9052e3980354a1bef633c7009db1393222450fb2
Satyakumar BS <bs.satya@gmail.com>
2018-11-03 19:53:43 +0530

    changed the sum implimentation to accept slice and modified the tests

--- learn_go_with_tests/sum.go
+++ learn_go_with_tests/sum.go
@@ -1,13 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
-
 // func Sum takes five integers and returns the sume of all the 5 integers
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	for _, n := range numbers {
-		fmt.Println(sum)
 		sum += n
 	}
 	return
--- learn_go_with_tests/sum_test.go
+++ learn_go_with_tests/sum_test.go
@@ -3,11 +3,14 @@ package main
 import "testing"
 
 func Test_Sum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	sum := Sum(numbers)
-	expected := 15
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		sum := Sum(numbers)
+		expected := 6
 
-	if sum != expected {
-		t.Errorf("Sum '%d', Expected '%d'", sum, expected)
-	}
+		if sum != expected {
+			t.Errorf("Sum '%d', Expected '%d'", sum, expected)
+		}
+	})
+
 }
