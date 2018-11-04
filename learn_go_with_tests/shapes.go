3d44490440ed9acf13653738abe86c9d90438adb
Satyakumar BS <bs.satya@gmail.com>
2018-11-04 22:08:59 +0530

    refactored the code, implimented interface, shape and methods to find area of rectange and circle

--- learn_go_with_tests/shapes.go
+++ learn_go_with_tests/shapes.go
@@ -1,12 +1,36 @@
 package main
 
-func Perimeter(width, height float64) (perimeter float64) {
-	perimeter = 2*width + 2*height
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+type Rectange struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectange) Area() (area float64) {
+	area = r.Height * r.Width
+	return
+}
+
+func (c Circle) Area() (area float64) {
+	area = math.Pi * c.Radius * c.Radius
+	return
+}
+
+func Perimeter(rectangle Rectange) (perimeter float64) {
+	perimeter = 2*rectangle.Width + 2*rectangle.Height
 
 	return
 }
 
-func Area(width, height float64) (area float64) {
-	area = width * height
+func Area(rectangle Rectange) (area float64) {
+	area = rectangle.Height * rectangle.Width
 	return
 }
--- learn_go_with_tests/shapes_test.go
+++ learn_go_with_tests/shapes_test.go
@@ -5,19 +5,32 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectange := Rectange{10.0, 10.0}
+	got := Perimeter(rectange)
 	want := 40.0
 
 	if got != want {
-		t.Errorf("Got (%v), Want (%v)", got, want)
+		t.Errorf("Got (%0.2f), Want (%0.2f)", got, want)
 	}
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 
-	if got != want {
-		t.Errorf("Got (%v), Want (%v)", got, want)
+		if got != want {
+			t.Errorf("Got (%0.2f), Want (%0.2f)", got, want)
+		}
 	}
+
+	t.Run("Area of Rectange", func(t *testing.T) {
+		rectangle := Rectange{10.0, 10.0}
+		checkArea(t, rectangle, 100.0)
+	})
+
+	t.Run("Area of Circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		checkArea(t, circle, 314.16)
+	})
 }
