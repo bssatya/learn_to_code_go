b28b6aece1f351721abcd11b052843ab50770668
Satyakumar BS <bs.satya@gmail.com>
2018-11-05 23:06:46 +0530

    Refactored the test code by creating assertion helper function in the test code

--- learn_go_with_tests/wallet_test.go
+++ learn_go_with_tests/wallet_test.go
@@ -6,30 +6,28 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	t.Run("Deposit", func (t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
 
 		got := wallet.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("Got (%s), want (%s)", got, want)
-		}
-	})
-	
-	t.Run("Withdraw", func (t *testing.T) {
-		wallet := Wallet{balance: Bitcoin(20)}
-
-		wallet.Withdraw(10)
-		got := wallet.Balance()
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("Got (%s), Want (%s)", got, want)
 		}
+	}
 
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(10)
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 }
