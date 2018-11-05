a17dd4dc07f5062c7a49fbb8bf3d55a842a8261c
Satyakumar BS <bs.satya@gmail.com>
2018-11-05 22:38:10 +0530

    Added tests to test wallet balance and Deposit functions

---
+++ learn_go_with_tests/wallet_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+
+	if got != want {
+		t.Errorf("Got (%v), want (%v)", got, want)
+	}
+
+}
