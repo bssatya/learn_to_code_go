44f986eae9fc2e0a387ec18b3dfb75984a9d8797
Satyakumar BS <bs.satya@gmail.com>
2018-11-05 22:41:18 +0530

    added balance member variable in wallet struct, implimented deposit and balance methods

--- learn_go_with_tests/wallet.go
+++ learn_go_with_tests/wallet.go
@@ -1,13 +1,14 @@
 package main
 
 type Wallet struct {
+	balance int
 }
 
 func (w Wallet) Deposit(ammount int) {
-
+	w.balance += ammount
 }
 
 func (w Wallet) Balance() (balance int) {
-
+	balance = w.balance
 	return
 }
