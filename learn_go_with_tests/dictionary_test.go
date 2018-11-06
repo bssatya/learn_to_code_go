ff68c89501fea13c132812942769b36df69a2ac3
Satyakumar BS <bs.satya@gmail.com>
2018-11-06 23:42:40 +0530

    added tests to search both known and unknows keys in the dictionary

--- learn_go_with_tests/dictionary_test.go
+++ learn_go_with_tests/dictionary_test.go
@@ -5,10 +5,19 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just testing"}
 
-	got := dictionary.Search(dictionary, "test")
-	want := "this is just testing"
+	t.Run("Known word", func(t *testing.T) {
+		got := dictionary.Search("test")
+		want := "this is just testing"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("Unknown word", func(t *testing.T) {
+		got := dictionary.Search("search")
+		want := ""
+
+		assertStrings(t, got, want)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
