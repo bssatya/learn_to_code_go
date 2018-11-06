ff93023c14c1d11f498fb38e657e344043cb7b23
Satyakumar BS <bs.satya@gmail.com>
2018-11-06 23:38:50 +0530

    added Dictionary type and implemented Search method

--- learn_go_with_tests/dictionary.go
+++ learn_go_with_tests/dictionary.go
@@ -1,5 +1,7 @@
 package main
 
-func Search(dictionary map[string]string, key string) string {
-	return dictionary[key]
+type Dictionary map[string]string
+
+func (d Dictionary) Search(dictionary map[string]string, key string) string {
+	return d[key]
 }
--- learn_go_with_tests/dictionary_test.go
+++ learn_go_with_tests/dictionary_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just testing"}
+	dictionary := Dictionary{"test": "this is just testing"}
 
-	got := Search(dictionary, "test")
+	got := dictionary.Search(dictionary, "test")
 	want := "this is just testing"
 
 	assertStrings(t, got, want)
