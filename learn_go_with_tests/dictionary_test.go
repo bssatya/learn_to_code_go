f16dc51f3ecbbae2763765160c6d1060c5c54b9c
Satyakumar BS <bs.satya@gmail.com>
2018-11-06 23:35:26 +0530

    refactored the test code by adding assertStrings function

--- learn_go_with_tests/dictionary_test.go
+++ learn_go_with_tests/dictionary_test.go
@@ -8,6 +8,12 @@ func TestSearch(t *testing.T) {
 	got := Search(dictionary, "test")
 	want := "this is just testing"
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
+
 	if got != want {
 		t.Errorf("Got (%s) Want (%s), Given (%s)", got, want, "test")
 	}
