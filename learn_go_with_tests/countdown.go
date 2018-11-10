17c9c1253dbb63159fb0e2ad7bb66e747223382f
Satyakumar BS <bs.satya@gmail.com>
2018-11-10 10:31:58 +0530

    making the countdown dramatic, added a sleep of 1 sec between each countdown

--- learn_go_with_tests/countdown.go
+++ learn_go_with_tests/countdown.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 const finalWord = "Go!"
@@ -10,7 +11,9 @@ const countdownStart = 3
 
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
+	time.Sleep(1 * time.Second)
 	fmt.Fprintf(out, finalWord)
 }
