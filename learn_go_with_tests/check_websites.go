2e41516bc221e883cc181c0cfb48cabeb4a56aef
Satya Kumar <bssatya@gmail.com>
2018-11-14 21:34:59 +0530

    added a function which will check the list of url's supplied to it

---
+++ learn_go_with_tests/check_websites.go
@@ -0,0 +1,13 @@
+package concurrency
+
+type websiteChecker func(string) bool
+
+func CheckWebsites(wc websiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		results[url] = wc(url)
+	}
+
+	return results
+}
