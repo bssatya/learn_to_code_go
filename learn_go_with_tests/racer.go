f9a2fa4ad7dbed94f190228b60e044578d0188cf
Satya Kumar <bssatya@gmail.com>
2018-11-16 05:24:29 +0530

    refactored the code to use select and channels

--- learn_go_with_tests/racer.go
+++ learn_go_with_tests/racer.go
@@ -2,22 +2,36 @@ package main
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) string {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
+	// aDuration := measureResponseTime(a)
+	// bDuration := measureResponseTime(b)
 
-	if aDuration < bDuration {
+	// if aDuration < bDuration {
+	// 	return a
+	// }
+
+	// return b
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
+
+// func measureResponseTime(url string) time.Duration {
+// 	start := time.Now()
+// 	http.Get(url)
+// 	return time.Since(start)
+// }
