84a64452c34525573acf34acc0762052d9b5d0c7
Satyakumar BS <bs.satya@gmail.com>
2018-11-17 23:16:10 +0530

    refactored the code to impliment timeout, and test the timeout implimentaiton

--- learn_go_with_tests/racer.go
+++ learn_go_with_tests/racer.go
@@ -1,23 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
-func Racer(a, b string) string {
-	// aDuration := measureResponseTime(a)
-	// bDuration := measureResponseTime(b)
+var tenSecondTimeout = 10 * time.Second
 
-	// if aDuration < bDuration {
-	// 	return a
-	// }
+func Racer(a, b string) (string, error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
 
-	// return b
+func ConfigurableRacer(a, b string, timeout time.Duration) (string, error) {
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
--- learn_go_with_tests/racer_test.go
+++ learn_go_with_tests/racer_test.go
@@ -8,22 +8,37 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("check ping", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
+		want := fastURL
 
-	got := Racer(slowURL, fastURL)
+		got, _ := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("Got (%s), Want (%s)", got, want)
-	}
+		if got != want {
+			t.Errorf("Got (%s), Want (%s)", got, want)
+		}
+	})
+
+	t.Run("returns an error if the server doesnt respond in 10sec", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didnt get one")
+		}
+
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
