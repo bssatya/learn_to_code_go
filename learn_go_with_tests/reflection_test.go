e0688d8527b6a967d12e00b354066c805f8e35e9
Satyakumar BS <bs.satya@gmail.com>
2018-11-18 15:26:21 +0530

    added first sad path test to fail the tests

--- learn_go_with_tests/reflection_test.go
+++ learn_go_with_tests/reflection_test.go
@@ -19,4 +19,9 @@ func TestWalk(t *testing.T) {
 	if len(got) != 1 {
 		t.Errorf("wrong no. of function calls, got %d want %d", len(got), 1)
 	}
+
+	if got[0] != expected {
+		t.Errorf("got %s, want %s", got[0], expected)
+	}
+
 }
