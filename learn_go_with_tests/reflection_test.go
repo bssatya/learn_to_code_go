0129e377a448697b1ae59393f471fec4f32d3c38
Satyakumar BS <bs.satya@gmail.com>
2018-11-18 15:21:33 +0530

    added tests for walk functionality

---
+++ learn_go_with_tests/reflection_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	expected := "Chris"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong no. of function calls, got %d want %d", len(got), 1)
+	}
+}
