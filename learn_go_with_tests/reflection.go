cc2d10981a1cf7afd9a018751755d6f85b5d587a
Satyakumar BS <bs.satya@gmail.com>
2018-11-18 15:27:55 +0530

    refactored the code with the right implimentation, tests passing

--- learn_go_with_tests/reflection.go
+++ learn_go_with_tests/reflection.go
@@ -1,5 +1,10 @@
 package main
 
+import "reflect"
+
 func walk(x interface{}, fn func(input string)) {
-	fn("I still cant believe, souch korea beat Genrmeny 2-0 to push them to the bottom of their group")
+	// fn("I still cant believe, souch korea beat Genrmeny 2-0 to push them to the bottom of their group")
+	val := reflect.ValueOf(x)
+	field := val.Field(0)
+	fn(field.String())
 }
