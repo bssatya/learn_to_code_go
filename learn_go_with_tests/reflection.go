1962c0e11a121b1cca1d80ff0628b5f0dc8c2701
Satya Kumar <bssatya@gmail.com>
2018-11-19 21:51:49 +0530

    addded just enough code to pass the tests

--- learn_go_with_tests/reflection.go
+++ learn_go_with_tests/reflection.go
@@ -6,6 +6,13 @@ func walk(x interface{}, fn func(input string)) {
 	// fn("I still cant believe, souch korea beat Genrmeny 2-0 to push them to the bottom of their group")
 	val := getValue(x)
 
+	if val.Kind() == reflect.Slice {
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+		return
+	}
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
