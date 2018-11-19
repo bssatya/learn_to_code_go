1c9ac4fb00100290627916a7d8ad60ea7f50a525
Satya Kumar <bssatya@gmail.com>
2018-11-19 21:42:52 +0530

    added just enough implimentation to pass the tests

--- learn_go_with_tests/reflection.go
+++ learn_go_with_tests/reflection.go
@@ -6,6 +6,10 @@ func walk(x interface{}, fn func(input string)) {
 	// fn("I still cant believe, souch korea beat Genrmeny 2-0 to push them to the bottom of their group")
 	val := reflect.ValueOf(x)
 
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
