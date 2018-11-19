34037d31152c019e10c816f47e9b38db0a4cd30c
Satya Kumar <bssatya@gmail.com>
2018-11-19 21:29:16 +0530

    refactored the code to remove the commented code

--- learn_go_with_tests/reflection_test.go
+++ learn_go_with_tests/reflection_test.go
@@ -6,25 +6,6 @@ import (
 )
 
 func TestWalk(t *testing.T) {
-	// expected := "Chris"
-	// var got []string
-
-	// x := struct {
-	// 	Name string
-	// }{expected}
-
-	// walk(x, func(input string) {
-	// 	got = append(got, input)
-	// })
-
-	// if len(got) != 1 {
-	// 	t.Errorf("wrong no. of function calls, got %d want %d", len(got), 1)
-	// }
-
-	// if got[0] != expected {
-	// 	t.Errorf("got %s, want %s", got[0], expected)
-	// }
-
 	cases := []struct {
 		Name          string
 		Input         interface{}
