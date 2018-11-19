460bfe5c792fe0405c80a3fa1c9a175032cf6f8f
Satya Kumar <bssatya@gmail.com>
2018-11-19 21:27:03 +0530

    added one more sad path tests, non string field

--- learn_go_with_tests/reflection_test.go
+++ learn_go_with_tests/reflection_test.go
@@ -6,24 +6,24 @@ import (
 )
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
+	// expected := "Chris"
+	// var got []string
 
-	x := struct {
-		Name string
-	}{expected}
+	// x := struct {
+	// 	Name string
+	// }{expected}
 
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
+	// walk(x, func(input string) {
+	// 	got = append(got, input)
+	// })
 
-	if len(got) != 1 {
-		t.Errorf("wrong no. of function calls, got %d want %d", len(got), 1)
-	}
+	// if len(got) != 1 {
+	// 	t.Errorf("wrong no. of function calls, got %d want %d", len(got), 1)
+	// }
 
-	if got[0] != expected {
-		t.Errorf("got %s, want %s", got[0], expected)
-	}
+	// if got[0] != expected {
+	// 	t.Errorf("got %s, want %s", got[0], expected)
+	// }
 
 	cases := []struct {
 		Name          string
@@ -44,6 +44,14 @@ func TestWalk(t *testing.T) {
 				City string
 			}{"Chris", "London"},
 			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
 		},
 	}
 
