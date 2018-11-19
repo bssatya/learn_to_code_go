1a4cfc7601bdc4301b093e4cb3bde49b2b51e495
Satya Kumar <bssatya@gmail.com>
2018-11-19 22:02:59 +0530

    further refactoring

--- learn_go_with_tests/reflection.go
+++ learn_go_with_tests/reflection.go
@@ -6,17 +6,21 @@ func walk(x interface{}, fn func(input string)) {
 	// fn("I still cant believe, souch korea beat Genrmeny 2-0 to push them to the bottom of their group")
 	val := getValue(x)
 
+	numberOfValues := 0
+	var getField func(int) reflect.Value
 	switch val.Kind() {
 	case reflect.String:
 		fn(val.String())
 	case reflect.Slice:
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
+		numberOfValues = val.Len()
+		getField = val.Index
 	case reflect.Struct:
-		for i := 0; i < val.NumField(); i++ {
-			walk(val.Field(i).Interface(), fn)
-		}
+		numberOfValues = val.NumField()
+		getField = val.Field
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
 	}
 }
 
