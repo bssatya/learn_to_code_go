717f9588a2ed9fa6a0eadfc2692b1653c5c01b91
Satya Kumar <bssatya@gmail.com>
2018-11-19 21:39:36 +0530

    refactored both the test code and the implimentation

--- learn_go_with_tests/reflection_test.go
+++ learn_go_with_tests/reflection_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 	cases := []struct {
 		Name          string
@@ -36,16 +46,10 @@ func TestWalk(t *testing.T) {
 		},
 		{
 			"Nested fields",
-			struct {
-				Name    string
-				Profile struct {
-					Age  int
-					City string
-				}
-			}{"Chris", struct {
-				Age  int
-				City string
-			}{33, "London"}},
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
 			[]string{"Chris", "London"},
 		},
 	}
