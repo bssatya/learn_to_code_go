a317daea63ea203d2ca0e347bbd24557b6dbebd7
Satya Kumar <bssatya@gmail.com>
2018-11-19 21:19:03 +0530

    refactored the test code to do table driven testing

--- learn_go_with_tests/reflection_test.go
+++ learn_go_with_tests/reflection_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -24,4 +25,30 @@ func TestWalk(t *testing.T) {
 		t.Errorf("got %s, want %s", got[0], expected)
 	}
 
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("Got %v, Want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
 }
