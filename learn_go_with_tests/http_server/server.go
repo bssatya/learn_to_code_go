75ceb83a1b2ad137c9a3c398b48d9621214c41f4
Satya Kumar <bssatya@gmail.com>
2018-11-21 22:06:18 +0530

    refactored the code to separate getting the score for a player from a different function

--- learn_go_with_tests/http_server/server.go
+++ learn_go_with_tests/http_server/server.go
@@ -7,14 +7,16 @@ import (
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
+	fmt.Fprintf(w, GetPlayerScore(player))
+}
 
-	if player == "Pepper" {
-		fmt.Fprintf(w, "20")
-		return
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
 	}
 
-	if player == "Floyd" {
-		fmt.Fprintf(w, "10")
-		return
+	if name == "Floyd" {
+		return "10"
 	}
+	return ""
 }
