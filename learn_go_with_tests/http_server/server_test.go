3ca2a7791ccad25dd3774731fb5831a3fc1fb550
Satyakumar BS <bs.satya@gmail.com>
2018-11-23 00:11:52 +0530

    added integration tests for postgres store

--- learn_go_with_tests/http_server/server_test.go
+++ learn_go_with_tests/http_server/server_test.go
@@ -110,6 +110,23 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	assertResponseBody(t, response.Body.String(), "3")
 }
 
+func TestRecordingWinsAndRetrievingThemFromPostgresStore(t *testing.T) {
+	store := NewPostgresPlayerStore()
+	server := PlayerServer{store}
+	player := "Joney"
+
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetScoreRequest(player))
+	assertStatus(t, response.Code, http.StatusOK)
+
+	assertResponseBody(t, response.Body.String(), "4")
+}
+
 func newGetScoreRequest(name string) *http.Request {
 	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/Players/%s", name), nil)
 	return request
