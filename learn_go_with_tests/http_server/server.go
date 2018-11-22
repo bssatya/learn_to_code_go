1204ebe766c91f133ec4af6c1120f5d97ef36e55
Satya Kumar <bssatya@gmail.com>
2018-11-22 21:43:18 +0530

    added enough code to pass the tests

--- learn_go_with_tests/http_server/server.go
+++ learn_go_with_tests/http_server/server.go
@@ -18,7 +18,7 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case http.MethodPost:
-		p.processWin(w)
+		p.processWin(w, r)
 	case http.MethodGet:
 		p.showScore(w, r)
 	}
@@ -36,8 +36,9 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter) {
-	p.store.RecordWin("Bob")
+func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+	p.store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
 
