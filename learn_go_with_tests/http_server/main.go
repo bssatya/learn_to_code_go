abda7e1fc4565566dc8550650eb61e8a8f9bfb9c
Satya Kumar <bssatya@gmail.com>
2018-11-22 13:18:51 +0530

    added a in memeory store and passing the reference to PlayerServer

--- learn_go_with_tests/http_server/main.go
+++ learn_go_with_tests/http_server/main.go
@@ -5,8 +5,15 @@ import (
 	"net/http"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	server := &PlayerServer{}
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
