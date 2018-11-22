6e74aaa40cf06f7893e9b77e0f148aaf388dfea8
Satya Kumar <bssatya@gmail.com>
2018-11-22 21:42:58 +0530

    added tests to verify the player stored in the store after the post

--- learn_go_with_tests/http_server/server_test.go
+++ learn_go_with_tests/http_server/server_test.go
@@ -75,7 +75,9 @@ func TestStoreWins(t *testing.T) {
 	server := &PlayerServer{&store}
 
 	t.Run("it returns accepted on POST", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodPost, "/Players/Pepper", nil)
+		player := "Pepper"
+
+		request := newPostWinRequest(player)
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
@@ -85,11 +87,20 @@ func TestStoreWins(t *testing.T) {
 		if len(store.winCalls) != 1 {
 			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
 		}
+
+		if store.winCalls[0] != player {
+			t.Errorf("did not store correct winner got %s, want %s", store.winCalls[0], player)
+		}
 	})
 }
 
 func newGetScoreRequest(name string) *http.Request {
-	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/Players/%s", name), nil)
+	return request
+}
+
+func newPostWinRequest(name string) *http.Request {
+	request, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/Players/%s", name), nil)
 	return request
 }
 
