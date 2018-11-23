9dd74d5abd13b0368c63fb90ff5e07b2c75c1b79
Satya Kumar <bssatya@gmail.com>
2018-11-23 21:04:03 +0530

    added tests to test /league path

--- learn_go_with_tests/http_server/server_test.go
+++ learn_go_with_tests/http_server/server_test.go
@@ -94,6 +94,20 @@ func TestStoreWins(t *testing.T) {
 	})
 }
 
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns 200 on /league", func (t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
+
 func newGetScoreRequest(name string) *http.Request {
 	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/Players/%s", name), nil)
 	return request
