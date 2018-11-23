5c25594b5a48c6652a8bf2107fbef55510edbc8a
Satya Kumar <bssatya@gmail.com>
2018-11-23 21:20:15 +0530

    serveHTTP is looking very big, refactored the code to separate the handler functions from serveHTTP functions

--- learn_go_with_tests/http_server/server.go
+++ learn_go_with_tests/http_server/server.go
@@ -18,21 +18,25 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	router := http.NewServeMux()
 
-	router.Handle("/league", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
 
-	router.Handle("/players/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		player := r.URL.Path[len("/players/"):]
-		switch r.Method {
-		case http.MethodPost:
-			p.processWin(w, player)
-		case http.MethodGet:
-			p.showScore(w, player)
-		}
-	}))
+	router.Handle("/players/", http.HandlerFunc(p.playerHandler))
 
 	router.ServeHTTP(w, r)
+}
+func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func (p *PlayerServer) playerHandler(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
 }
 
 func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
