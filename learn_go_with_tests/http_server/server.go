081bab6c4bb2b96cc33dcc67da30113228eec4c1
Satya Kumar <bssatya@gmail.com>
2018-11-25 18:34:46 +0530

    modified the League handler to add the content-type header

--- learn_go_with_tests/http_server/server.go
+++ learn_go_with_tests/http_server/server.go
@@ -38,6 +38,7 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 }
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
 	json.NewEncoder(w).Encode(p.store.GetLeague())
 
 	w.WriteHeader(http.StatusOK)
