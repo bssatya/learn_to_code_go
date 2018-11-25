dd84c404b79d32fad8915536a9f79771a90958c3
Satya Kumar <bssatya@gmail.com>
2018-11-25 14:07:17 +0530

    added tests to decode the jscon coming in as part of the response body

--- learn_go_with_tests/http_server/server_test.go
+++ learn_go_with_tests/http_server/server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -98,12 +99,19 @@ func TestLeague(t *testing.T) {
 	store := StubPlayerStore{}
 	server := NewPlayerServer(&store)
 
-	t.Run("it returns 200 on /league", func (t *testing.T) {
+	t.Run("it returns 200 on /league", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
+		var got []Player
+
+		err := json.NewDecoder(response.Body).Decode(&got)
+
+		if err != nil {
+			t.Fatalf("Unable to parse response from server (%s) in to slice of Player, (%v)", response.Body, err)
+		}
 		assertStatus(t, response.Code, http.StatusOK)
 	})
 }
