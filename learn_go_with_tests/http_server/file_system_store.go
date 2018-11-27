c104e24c03e45c1e6f7d9b26a5c1aa594f9cc720
Satya Kumar <bssatya@gmail.com>
2018-11-27 21:20:04 +0530

    refactored the getleague function

--- learn_go_with_tests/http_server/file_system_store.go
+++ learn_go_with_tests/http_server/file_system_store.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"io"
 )
 
@@ -11,8 +10,7 @@ type FileSystemPlayerStore struct {
 
 func (f *FileSystemPlayerStore) GetLeague() []Player {
 
-	var league []Player
-	json.NewDecoder(f.database).Decode(&league)
+	league, _ := NewLeague(f.database)
 
 	return league
 }
