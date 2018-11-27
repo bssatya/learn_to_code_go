e2ba21dc62e3c21d1002a9c81a55c151396fb815
Satya Kumar <bssatya@gmail.com>
2018-11-27 21:18:35 +0530

    refactored the code, creating a new league post json decoding is move to league.go file

---
+++ learn_go_with_tests/http_server/league.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+func NewLeague(rdr io.Reader) ([]Player, error) {
+	var league []Player
+
+	err := json.NewDecoder(rdr).Decode(&league)
+	if err != nil {
+		err = fmt.Errorf("Problem parsing the league, %v", err)
+	}
+
+	return league, err
+}
