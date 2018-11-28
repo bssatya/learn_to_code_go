bddec6327c7b3e76070f88743c91cc078d10d47b
Satya Kumar <bssatya@gmail.com>
2018-11-28 21:55:40 +0530

    done some refactoring of the test code

--- learn_go_with_tests/http_server/file_system_store_test.go
+++ learn_go_with_tests/http_server/file_system_store_test.go
@@ -31,8 +31,12 @@ func TestFileSystemStore(t *testing.T) {
 
 		want := 33
 
-		if got != want {
-			t.Errorf("got %d want %d", got, want)
-		}
+		assertScoreEquals(t, got, want)
 	})
+}
+
+func assertScoreEquals(t *testing.T, got, want int) {
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
 }
