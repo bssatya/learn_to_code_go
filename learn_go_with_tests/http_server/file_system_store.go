545cadfa7ba97fadea1e45f52e795970d7a59f1b
Satyakumar BS <bs.satya@gmail.com>
2018-11-30 22:57:37 +0530

    refactored the GetPlayerScore and RecordWin methods to use Find method

--- learn_go_with_tests/http_server/file_system_store.go
+++ learn_go_with_tests/http_server/file_system_store.go
@@ -17,25 +17,22 @@ func (f *FileSystemPlayerStore) GetLeague() []Player {
 }
 
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
-	var wins int
 
-	for _, player := range f.GetLeague() {
-		if player.Name == name {
-			wins = player.Wins
-			break
-		}
+	player := f.GetLeague().Find(name)
+
+	if player != nil {
+		return player.Wins
 	}
 
-	return wins
+	return 0
 }
 
 func (f *FileSystemPlayerStore) RecordWin(name string) {
 	league := f.GetLeague()
+	player := league.Find(name)
 
-	for i, player := range league {
-		if player.Name == name {
-			league[i].Wins++
-		}
+	if player != nil {
+		player.Wins++
 	}
 
 	f.database.Seek(0, 0)
