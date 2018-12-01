f9b24cff295e396a3fb545f16f3f0b58ade9c872
Satyakumar BS <bs.satya@gmail.com>
2018-12-01 23:47:47 +0530

    refactored the main program to use the file system store

--- learn_go_with_tests/http_server/main.go
+++ learn_go_with_tests/http_server/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
-	server := NewPlayerServer(NewPostgresPlayerStore())
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("Problem opening %s %v", dbFileName, err)
+	}
+
+	store := &FileSystemPlayerStore{db}
+	server := NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
