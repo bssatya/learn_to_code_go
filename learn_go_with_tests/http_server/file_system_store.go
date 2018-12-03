72b1ef46033a57531ff11277c89ec711ee50e463
Satyakumar BS <bs.satya@gmail.com>
2018-12-03 22:33:11 +0530

    refactored the code to support tape

--- learn_go_with_tests/http_server/file_system_store.go
+++ learn_go_with_tests/http_server/file_system_store.go
@@ -3,18 +3,22 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"os"
 )
 
 type FileSystemPlayerStore struct {
-	database io.ReadWriteSeeker
+	database io.Writer
 	league   League
 }
 
-func NewFileSystemPlayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStore {
+func NewFileSystemPlayerStore(database *os.File) *FileSystemPlayerStore {
 	database.Seek(0, 0)
 	league, _ := NewLeague(database)
 
-	return &FileSystemPlayerStore{database: database, league: league}
+	return &FileSystemPlayerStore{
+		database: &tape{database},
+		league:   league,
+	}
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
@@ -40,8 +44,6 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 	} else {
 		f.league = append(f.league, Player{name, 1})
 	}
-
-	f.database.Seek(0, 0)
 
 	json.NewEncoder(f.database).Encode(f.league)
 }
