332b09bdde488b458305a637808951cd01d7aa7d
Satyakumar BS <bs.satya@gmail.com>
2018-12-04 22:32:23 +0530

    refactored the code to handle the error condition in creating the leagu during creation of new file system player store

--- learn_go_with_tests/http_server/file_system_store.go
+++ learn_go_with_tests/http_server/file_system_store.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 )
 
@@ -10,14 +11,18 @@ type FileSystemPlayerStore struct {
 	league   League
 }
 
-func NewFileSystemPlayerStore(file *os.File) *FileSystemPlayerStore {
+func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 	file.Seek(0, 0)
-	league, _ := NewLeague(file)
+	league, err := NewLeague(file)
+
+	if err != nil {
+		return nil, fmt.Errorf("Problem loading the player store from file %s, %v", file.Name(), err)
+	}
 
 	return &FileSystemPlayerStore{
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
-	}
+	}, nil
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
