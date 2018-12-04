c60d903474c7eb949549f3c1dfc5d8230bab148d
Satyakumar BS <bs.satya@gmail.com>
2018-12-04 22:33:14 +0530

    done required changes to support the changes done in the newfilesystemplayrstore

--- learn_go_with_tests/http_server/main.go
+++ learn_go_with_tests/http_server/main.go
@@ -14,7 +14,10 @@ func main() {
 		log.Fatalf("Problem opening %s %v", dbFileName, err)
 	}
 
-	store := NewFileSystemPlayerStore(db)
+	store, err := NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatalf("problem creating a file system player store, %v", err)
+	}
 	server := NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
