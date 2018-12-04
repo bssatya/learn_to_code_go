59392852d29aeefebd29f9e0702539e371163d0f
Satyakumar BS <bs.satya@gmail.com>
2018-12-04 23:08:14 +0530

    added enough code to sort the slice before returning

--- learn_go_with_tests/http_server/file_system_store.go
+++ learn_go_with_tests/http_server/file_system_store.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 )
 
 type FileSystemPlayerStore struct {
@@ -47,6 +48,10 @@ func initialisePlayerDBfile(file *os.File) error {
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
+
+	sort.Slice(f.league, func(i, j int) bool {
+		return f.league[i].Wins > f.league[j].Wins
+	})
 	return f.league
 }
 
