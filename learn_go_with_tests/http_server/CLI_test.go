e0cf70afb73b7f450436a2b431c3744934413bd4
Satya Kumar <satya.kumar@COMVIVA.COM>
2018-12-06 21:34:35 +0530

    extended the test to input the string and test recording the winner

--- learn_go_with_tests/http_server/CLI_test.go
+++ learn_go_with_tests/http_server/CLI_test.go
@@ -1,14 +1,26 @@
 package poker
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCLI(t *testing.T) {
+	in := strings.NewReader("Chris wins\n")
 	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore}
+
+	cli := &CLI{playerStore, in}
 
 	cli.PlayPoker()
 
 	if len(playerStore.winCalls) != 1 {
 		t.Fatal("expected a win call but didnt get any")
+	}
+
+	got := playerStore.winCalls[0]
+	want := "Chris"
+
+	if got != want {
+		t.Errorf("didnt record correct winner, got %s, want %s", got, want)
 	}
 }
