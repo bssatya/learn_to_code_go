1a62856d2dfd7c1cb8eeb8a80de7370025e73a89
Satya Kumar <satya.kumar@COMVIVA.COM>
2018-12-06 21:18:31 +0530

    added tests for CLI

--- learn_go_with_tests/http_server/cmd/cli/CLI_test.go
+++ learn_go_with_tests/http_server/cmd/cli/CLI_test.go
@@ -3,5 +3,12 @@ package main
 import "testing"
 
 func TestCLI(t *testing.T) {
+	playerStore := &StubPlayerStore{}
+	cli := &CLI{playerStore}
 
+	cli.PlayPoker()
+
+	if len(playerStore.winCalls) != 1 {
+		t.Fatal("expected a win call but didnt get any")
+	}
 }
