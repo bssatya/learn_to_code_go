e33dcc5f09c0ef6b3aa788a153c53f3fe21e0f68
Satya Kumar <satya.kumar@COMVIVA.COM>
2018-12-07 20:14:51 +0530

    added sufficiently enough code to pass the tests

--- learn_go_with_tests/http_server/cli.go
+++ learn_go_with_tests/http_server/cli.go
@@ -14,16 +14,19 @@ type BlindAlerter interface {
 type CLI struct {
 	playerStore PlayerStore
 	in          *bufio.Scanner
+	alerter     BlindAlerter
 }
 
-func NewCLI(store PlayerStore, in io.Reader, alert BlindAlerter) *CLI {
+func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
 	return &CLI{
 		playerStore: store,
 		in:          bufio.NewScanner(in),
+		alerter:     alerter,
 	}
 }
 
 func (cli *CLI) PlayPoker() {
+	cli.alerter.ScheduleAlertAt(5*time.Second, 100)
 	userInput := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(userInput))
 }
