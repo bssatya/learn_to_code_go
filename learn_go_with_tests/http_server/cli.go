c75ff4897df230df445e53d689686d1f2e1c75c9
Satyakumar BS <bs.satya@gmail.com>
2018-12-09 22:39:06 +0530

    refactored the code

--- learn_go_with_tests/http_server/cli.go
+++ learn_go_with_tests/http_server/cli.go
@@ -26,15 +26,18 @@ func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
 }
 
 func (cli *CLI) PlayPoker() {
+	cli.scheduleBlindAlerts()
+	userInput := cli.readLine()
+	cli.playerStore.RecordWin(extractWinner(userInput))
+}
+
+func (cli *CLI) scheduleBlindAlerts() {
 	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
 	blindTime := 0 * time.Second
 	for _, blind := range blinds {
 		cli.alerter.ScheduleAlertAt(blindTime, blind)
 		blindTime = blindTime + 10*time.Minute
 	}
-
-	userInput := cli.readLine()
-	cli.playerStore.RecordWin(extractWinner(userInput))
 }
 
 func extractWinner(userInput string) string {
