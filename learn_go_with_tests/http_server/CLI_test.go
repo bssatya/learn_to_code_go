2dbc3fbeeec5186ce474bd8bbaca1d1e3eed16dc
Satyakumar BS <bs.satya@gmail.com>
2018-12-09 22:31:58 +0530

    added tests to schedule printing of blind values

--- learn_go_with_tests/http_server/CLI_test.go
+++ learn_go_with_tests/http_server/CLI_test.go
@@ -1,24 +1,25 @@
 package poker_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 	"time"
 
-	"github.com/bssatya/learn_to_code_go/learn_go_with_tests/http_server"
+	"github.com/learn_to_code_go/learn_go_with_tests/http_server"
 )
 
 type SpyBlindAlerter struct {
 	alerts []struct {
 		scheduledAt time.Duration
-		ammount     int
+		amount      int
 	}
 }
 
 func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, ammount int) {
 	s.alerts = append(s.alerts, struct {
 		scheduledAt time.Duration
-		ammount     int
+		amount      int
 	}{duration, ammount})
 }
 
@@ -55,6 +56,53 @@ func TestCLI(t *testing.T) {
 
 		if len(blindAlerter.alerts) != 1 {
 			t.Fatalf("expected a blind alert to be scheduled")
+		}
+	})
+
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+		blindAlerter := &SpyBlindAlerter{}
+
+		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli.PlayPoker()
+
+		cases := []struct {
+			expectedScheduleTime time.Duration
+			expectedAmount       int
+		}{
+			{0 * time.Second, 100},
+			{10 * time.Minute, 200},
+			{20 * time.Minute, 300},
+			{30 * time.Minute, 400},
+			{40 * time.Minute, 500},
+			{50 * time.Minute, 600},
+			{60 * time.Minute, 800},
+			{70 * time.Minute, 1000},
+			{80 * time.Minute, 2000},
+			{90 * time.Minute, 4000},
+			{100 * time.Minute, 8000},
+		}
+
+		for i, c := range cases {
+			t.Run(fmt.Sprintf("%d scheduled for %v", c.expectedAmount, c.expectedScheduleTime), func(t *testing.T) {
+
+				if len(blindAlerter.alerts) <= i {
+					t.Fatalf("alert %d was not scheduled %v", i, blindAlerter.alerts)
+				}
+
+				alert := blindAlerter.alerts[i]
+
+				amountGot := alert.amount
+				if amountGot != c.expectedAmount {
+					t.Errorf("got amount %d, want %d", amountGot, c.expectedAmount)
+				}
+
+				gotScheduledTime := alert.scheduledAt
+				if gotScheduledTime != c.expectedScheduleTime {
+					t.Errorf("got scheduled time of %v, want %v", gotScheduledTime, c.expectedScheduleTime)
+				}
+			})
 		}
 	})
 
